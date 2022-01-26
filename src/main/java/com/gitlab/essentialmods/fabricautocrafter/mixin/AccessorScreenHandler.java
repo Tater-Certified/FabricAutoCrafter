e5bc2ab6a39bd0759a386f469e7fe1a1e1e2df66
KJP12 <kjp12.k.joy.programer12@gmail.com>
2022-01-26 17:49:01 -0600

    Fixes #1

---
+++ src/main/java/com/gitlab/essentialmods/fabricautocrafter/mixin/AccessorScreenHandler.java
@@ -0,0 +1,20 @@
+package com.gitlab.essentialmods.fabricautocrafter.mixin;// Created 2022-26-01T17:38:48
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author KJP12
+ * @since ${version}
+ **/
+@Mixin(ScreenHandler.class)
+public interface AccessorScreenHandler {
+    @Accessor
+    DefaultedList<ItemStack> getPreviousTrackedStacks();
+
+    @Accessor
+    DefaultedList<ItemStack> getTrackedStacks();
+}
